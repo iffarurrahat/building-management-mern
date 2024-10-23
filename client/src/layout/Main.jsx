883d743aae445148161dfba6704b214866e3bbb6
iffarurrahat <iffarurrahat@gmail.com>
2024-10-23 22:31:03 +0600

    a new functionality name is preloader

--- client/src/layout/Main.jsx
+++ client/src/layout/Main.jsx
@@ -1,15 +1,41 @@
 import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
+import Preloader from "../components/ui/Preloader/Preloader";
 
 const Main = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      // Hide the preloader after everything is loaded
+      setIsLoading(false);
+    };
+
+    // Add event listener for when the page is fully loaded
+    window.addEventListener("load", handleLoad);
+
+    return () => {
+      // Cleanup the event listener
+      window.removeEventListener("load", handleLoad);
+    };
+  }, []);
+
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <div className="flex-grow">
-        <Outlet />
-      </div>
-      <Footer />
+    <div>
+      {/* Show Preloader when isLoading is true */}
+      {isLoading && <Preloader />}
+
+      {!isLoading && (
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <div className="flex-grow">
+            <Outlet />
+          </div>
+          <Footer />
+        </div>
+      )}
     </div>
   );
 };
