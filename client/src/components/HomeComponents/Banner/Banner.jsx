1cbba0ebc1384d1c310d83257b465bdf81843d7d
iffarurrahat <iffarurrahat@gmail.com>
2024-10-23 22:20:49 +0600

    Banner changes some code for smoothly image loaded when anyone come home page

--- client/src/components/HomeComponents/Banner/Banner.jsx
+++ client/src/components/HomeComponents/Banner/Banner.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import Slider from "./Slider";
 import toast from "react-hot-toast";
+import Spinner from "../../ui/Spinner/Spinner";
 
 const Banner = () => {
   const [slides, setSlides] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const loadSlides = async () => {
@@ -11,12 +13,25 @@ const Banner = () => {
         const res = await fetch("slider.json");
         const data = await res.json();
         setSlides(data);
+        setLoading(false);
       } catch (error) {
         toast.error(error);
+        setLoading(false);
       }
     };
     loadSlides();
   }, []);
+
+  if (loading) {
+    // Render a loader while images are loading
+    return (
+      <div className="flex justify-center items-center h-[450px] sm:h-[550px] md:h-[700px] lg:h-screen">
+        <p className="text-white text-lg">
+          <Spinner />
+        </p>
+      </div>
+    );
+  }
 
   return (
     <div>
